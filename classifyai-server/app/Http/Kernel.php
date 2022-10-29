0514229218951e980907fa6124aa60d85a50df28
jessymatta <jessy.w.matta@gmail.com>
2022-10-29 17:39:39 +0300

    Backend ⇄ Add the route for the custom Supervisor middleware

--- classifyai-server/app/Http/Kernel.php
+++ classifyai-server/app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'super_supervisor.role' => \App\Http\Middleware\SuperSupervisorMiddleware::class,
+        'supervisor.role' => \App\Http\Middleware\SupervisorMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
