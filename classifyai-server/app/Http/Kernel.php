fba9eeb801da5f947d08cf04f46617771240835e
jessymatta <jessy.w.matta@gmail.com>
2022-10-29 17:41:29 +0300

    Backend ⇄ Add the route for the custom Operator middleware

--- classifyai-server/app/Http/Kernel.php
+++ classifyai-server/app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'super_supervisor.role' => \App\Http\Middleware\SuperSupervisorMiddleware::class,
         'supervisor.role' => \App\Http\Middleware\SupervisorMiddleware::class,
+        'operator.role' => \App\Http\Middleware\OperatorMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
