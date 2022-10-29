172d869eb0d2e8c7db48c0f0069c4ff593e05bfa
jessymatta <jessy.w.matta@gmail.com>
2022-10-29 15:54:37 +0300

    Backend ⇄ Add AuthController + users registration method

---
+++ classifyai-server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request, $user_role_id)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'username' => 'required|string|unique:users',
+            'dob' => 'required',
+            'password' => 'required|string|confirmed|min:7'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $role_to_find = Role::findOrFail($user_role_id);
+
+        $role_to_find->users()->create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'username' => $request->username,
+            'dob' => $request->dob,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'status' => "success",
+            'user' => $role_to_find
+        ], 201);
+        //TODO: handle profile pic upload
+
+    }
+}
