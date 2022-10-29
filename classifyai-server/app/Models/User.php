7b22e16b501852a03d722b04dd884679861aeae3
jessymatta <jessy.w.matta@gmail.com>
2022-10-29 15:47:23 +0300

    Backend ⇄ Add a method to link the User Model to the Role Model (1-m)

--- classifyai-server/app/Models/User.php
+++ classifyai-server/app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable implements JWTSubject
         'profile_pic_url',
         'dob',
         'is_deleted',
-        'user_role'
+        'role_id'
     ];
 
     /**
@@ -59,4 +59,8 @@ class User extends Authenticatable implements JWTSubject
         return [];
     }
 
+    public function role()
+    {
+        return $this->belongsTo(Role::class,'role');
+    }
 }
