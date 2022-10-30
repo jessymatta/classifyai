cbdecfa31f96a91aec06ea79352589833693c9df
jessymatta <jessy.w.matta@gmail.com>
2022-10-30 22:10:48 +0200

    Backend ⇄ Add a method to handle audio decoding and saving

--- classifyai-server/app/Http/Controllers/CallController.php
+++ classifyai-server/app/Http/Controllers/CallController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\File;
+
 
 class CallController extends Controller
 {
@@ -13,7 +15,7 @@ class CallController extends Controller
         $validator = Validator::make($request->all(), [
             'call_custom_id' => 'required|string',
             'cutomer_nbr' => 'required|string',
-            'audio_url' => 'required|string',
+            'base64_audio' => 'required|string',
             'duration' => 'required',
             'positive_emotions_pct' => 'required',
             'negative_emotions_pct' => 'required',
@@ -27,12 +29,14 @@ class CallController extends Controller
 
         $operator_to_find = User::findOrFail($request->operator_id);
 
-        // TODO : handle audio upload + sentiment analysis
+        // TODO : handle sentiment analysis
+        $audio_url = $this->uploadCall($request->base64_audio, $request->operator_id);
+
 
         $operator_to_find->calls()->create([
             'call_custom_id' => $request->call_custom_id,
             'cutomer_nbr' => $request->cutomer_nbr,
-            'audio_url' => $request->audio_url,
+            'audio_url' => $audio_url,
             'duration' => $request->duration,
             'positive_emotions_pct' => $request->positive_emotions_pct,
             'negative_emotions_pct' => $request->negative_emotions_pct,
@@ -46,4 +50,20 @@ class CallController extends Controller
         ], 201);
     }
 
+    public function uploadCall(String $base64_audio, $operator_id)
+    {
+
+        $audio_url = $operator_id . time() . ".mp3";
+        $replace = substr($base64_audio, 0, strpos($base64_audio, ',') + 1);
+        $image = str_replace($replace, '', $base64_audio);
+        $image = str_replace(' ', '+', $image);
+        $folder = public_path("calls_folder/" . $operator_id);
+
+        if (!File::exists($folder)); {
+            File::makeDirectory($folder, 0777, true, true);
+        }
+
+        file_put_contents(public_path("calls_folder/" . $operator_id . "/") . $audio_url, base64_decode($base64_audio));
+        return $audio_url;
+    }
 }
