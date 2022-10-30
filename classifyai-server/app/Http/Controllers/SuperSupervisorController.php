b1587594186d757e7ae98dd0e7730b4c5f42f8d0
jessymatta <jessy.w.matta@gmail.com>
2022-10-30 07:37:38 +0200

    Backend ⇄ Add a method to register operators

--- classifyai-server/app/Http/Controllers/SuperSupervisorController.php
+++ classifyai-server/app/Http/Controllers/SuperSupervisorController.php
@@ -17,4 +17,15 @@ class SuperSupervisorController extends Controller
         $res = app('App\Http\Controllers\AuthController')->register($request, $role_id);
         return $res;
     }
+
+    public function addOperator(Request $request)
+    {
+        $role_id_obj = Role::select('id')->where('role', 'OPERATOR')->first();
+        if (!$role_id_obj) {
+            return response()->json(['error' => 'Not a valid role'], 400);
+        }
+        $role_id = json_decode($role_id_obj)->id;
+        $res = app('App\Http\Controllers\AuthController')->register($request, $role_id);
+        return $res;
+    }
 }
