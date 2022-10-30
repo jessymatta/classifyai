6c02d3540438f18638a915493ea38f32d2dcbb88
jessymatta <jessy.w.matta@gmail.com>
2022-10-30 17:45:44 +0200

    Backend ⇄ Create the Call Model

---
+++ classifyai-server/app/Models/Call.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Call extends Model
+{
+    use HasFactory;
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'call_custom_id',
+        'cutomer_nbr',
+        'audio_url',
+        'duration',
+        'positive_emotions_pct',
+        'negative_emotions_pct',
+        'neutral_emotions_pct',
+        'operator_id'
+    ];
+
+}
