e353248ca80c657ed13ee71ac0db4bd8a83be134
jessymatta <jessy.w.matta@gmail.com>
2022-10-30 07:31:29 +0200

    Backend ⇄ Add a method to register supervisors

--- classifyai-server/app/Http/Controllers/SuperSupervisorController.php
+++ classifyai-server/app/Http/Controllers/SuperSupervisorController.php
@@ -2,7 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Models\Role;
+
 class SuperSupervisorController extends Controller
 {
-
+    public function addSupervisor(Request $request)
+    {
+        $role_id_obj = Role::select('id')->where('role', 'SUPERVISOR')->first();
+        if (!$role_id_obj) {
+            return response()->json(['error' => 'Not a valid role'], 400);
+        }
+        $role_id = json_decode($role_id_obj)->id;
+        $res = app('App\Http\Controllers\AuthController')->register($request, $role_id);
+        return $res;
+    }
 }
