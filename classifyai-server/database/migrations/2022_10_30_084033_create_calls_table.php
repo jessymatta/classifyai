7e7bcbfc6d990b974e656ad6eeb69b60d323f5e8
jessymatta <jessy.w.matta@gmail.com>
2022-10-30 10:56:36 +0200

    Backend ⇄ Create the 'calls' database table

---
+++ classifyai-server/database/migrations/2022_10_30_084033_create_calls_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calls', function (Blueprint $table) {
+            $table->id();
+            $table->string('call_custom_id');
+            $table->string('cutomer_nbr');
+            $table->string('audio_url');
+            $table->integer('duration');
+            $table->integer('positive_emotions_pct');
+            $table->integer('negative_emotions_pct');
+            $table->integer('neutral_emotions_pct');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calls');
+    }
+};
