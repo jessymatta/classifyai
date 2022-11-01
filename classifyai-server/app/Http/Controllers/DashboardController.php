cfcb4d0c50fca8878c8ccb9f93dfbba75b82a37e
jessymatta <jessy.w.matta@gmail.com>
2022-11-01 17:47:58 +0200

    Backend ⇄ Add a method that get the total number of operators

--- classifyai-server/app/Http/Controllers/DashboardController.php
+++ classifyai-server/app/Http/Controllers/DashboardController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
-    //
+    public function getOperatorsCount()
+    {
+        $operators_count = User::whereHas('role', function ($query) {
+            $query->where('role', 'OPERATOR');
+        })->where('is_deleted', false)->count();
+
+        return response()->json(['operators_count' => $operators_count], 200);
+
+    }
+
 }
