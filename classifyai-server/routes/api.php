51b75d5aa5c938c0f93510aac4fcc8b37cdb536e
jessymatta <jessy.w.matta@gmail.com>
2022-11-01 11:07:30 +0200

    Backend ⇄ Add a route to get all users with OPERATOR role

--- classifyai-server/routes/api.php
+++ classifyai-server/routes/api.php
@@ -16,6 +16,7 @@ Route::group(['prefix' => 'v0.1'], function () {
             Route::post('/supervisors', [SuperSupervisorController::class, 'addSupervisor'])->name('add-supervisor');
             Route::post('/operators', [SuperSupervisorController::class, 'addOperator'])->name('add-operator');
             Route::post('/calls', [CallController::class, 'addCall'])->name('add-call');
+            Route::get('/operators', [SuperSupervisorController::class, 'getOperators'])->name('get-operators');
         });
     });
 
