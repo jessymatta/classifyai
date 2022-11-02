05b875f63203fdfc2b0da53c6355f500fa2e3b24
jessymatta <jessy.w.matta@gmail.com>
2022-11-02 18:16:53 +0200

    Backend ⇄ Add a method in SuperSupervisor Service that adds operators

--- classifyai-server/app/Services/SuperSupervisorService.php
+++ classifyai-server/app/Services/SuperSupervisorService.php
@@ -23,4 +23,21 @@ class SuperSupervisorService
         $res = app('App\Http\Controllers\AuthController')->register($request, $role_id);
         return $res;
     }
+
+    /**
+     * Add a user to the 'users' database table with a OPERATOR role
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function handleAddOperator(Request $request)
+    {
+        $role_id_obj = Role::select('id')->where('role', 'OPERATOR')->first();
+        if (!$role_id_obj) {
+            return response()->json(['error' => 'Not a valid role'], 400);
+        }
+        $role_id = json_decode($role_id_obj)->id;
+        $res = app('App\Http\Controllers\AuthController')->register($request, $role_id);
+        return $res;
+    }
 }
