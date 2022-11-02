db94370678af46a2607ffac38352c46de4984129
jessymatta <jessy.w.matta@gmail.com>
2022-11-02 18:10:29 +0200

    Backend ⇄ Call the method to register Supervisors from the Service Class

--- classifyai-server/app/Http/Controllers/SuperSupervisorController.php
+++ classifyai-server/app/Http/Controllers/SuperSupervisorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\SuperSupervisorService;
 use Illuminate\Http\Request;
 use App\Models\Role;
 use App\Models\User;
@@ -11,14 +12,16 @@ use Illuminate\Support\Facades\Validator;
 
 class SuperSupervisorController extends Controller
 {
-    public function addSupervisor(Request $request)
+    /**
+     * Add a user to the 'users' database table with a SUPERVISOR role
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param App\Services\SuperSupervisorService $super_supervisor_service
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function addSupervisor(Request $request, SuperSupervisorService $superSupervisorService)
     {
-        $role_id_obj = Role::select('id')->where('role', 'SUPERVISOR')->first();
-        if (!$role_id_obj) {
-            return response()->json(['error' => 'Not a valid role'], 400);
-        }
-        $role_id = json_decode($role_id_obj)->id;
-        $res = app('App\Http\Controllers\AuthController')->register($request, $role_id);
+        $res = $superSupervisorService->handleAddSupervisor($request);
         return $res;
     }
 
