7606b4a445066eac7e5498015e28bc6064b3cab2
jessymatta <jessy.w.matta@gmail.com>
2022-11-03 16:04:51 +0200

    Backend ⇄ Add a method to get a script in the Script Service class

--- classifyai-server/app/Services/ScriptService.php
+++ classifyai-server/app/Services/ScriptService.php
@@ -2,7 +2,27 @@
 
 namespace App\Services;
 
+use App\Models\Call;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class ScriptService
 {
+    /**
+     * Get a call by id
+     *
+     * @param int $call_id
+     * @return String
+     */
+    public function handleGetCallScript(int $id)
+    {
+        try {
+            $call = Call::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            abort(response()->json(['error' => 'Call not found'], 400));
+        }
 
+        $script_url = $call->script_url;
+
+        return $script_url;
+    }
 }
