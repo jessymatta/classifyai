b9aeac2c0cb8f47835645d2834d965fdd6cd208d
jessymatta <jessy.w.matta@gmail.com>
2022-11-03 16:06:18 +0200

    Backend ⇄ Add a method to get a script in the Script Controller class

--- classifyai-server/app/Http/Controllers/ScriptController.php
+++ classifyai-server/app/Http/Controllers/ScriptController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ScriptService;
 use Illuminate\Http\Request;
 
 class ScriptController extends Controller
 {
-    //
+    /**
+     * Get a call script by id
+     *
+     * @param int $id
+     * @param ScriptService $scriptService
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getCallScript(int $id)
+    {
+        $scriptService = new ScriptService();
+        $script_url = $scriptService->handleGetCallScript($id);
+        return response()->json(['script_url' => $script_url], 200);
+    }
 }
