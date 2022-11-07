b03d3043b794c96dff06d6dc7042d71eb0525648
jessymatta <jessy.w.matta@gmail.com>
2022-11-07 06:59:47 +0200

    Frontend ⇄ Add the input component

---
+++ classifyai-frontend/src/components/input/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./index.scss";
+import { InputProps } from "./index";
+
+const Input = ({ type, label, required, value, onChange }: InputProps) => {
+    return (
+        <div className="form">
+            <input type={type} value={value} className="form__input" id="input" required={required} autoComplete="off" placeholder=" " onChange={onChange} />
+            <label htmlFor="input" className="form__label">{label}</label>
+        </div>
+    )
+}
+
+export default Input
