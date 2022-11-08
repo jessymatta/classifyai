ff32c3a70e93517d3f9e81466eab416e86e2dc7a
jessymatta <jessy.w.matta@gmail.com>
2022-11-08 14:47:33 +0200

    Frontend ⇄ Add some components to the Sidebar component

--- classifyai-frontend/src/components/sidebar/index.tsx
+++ classifyai-frontend/src/components/sidebar/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Logo from "../logo/";
+import "./index.scss"
+import { SidebarProps } from './sidebar';
 
-const Sidebar = () => {
+const Sidebar = ({role, username}:SidebarProps) => {
     return (
-        <div>index</div>
+        <div className="sidebar">
+            <div className="sidebar__top">
+                <Logo logoType={"sidebar__logo"} />
+            </div>
+                <p className='sidebar__role'>{role}</p>
+                <p className="sidebar__username">{username? `@${username}`: ""}</p>
+
+            <div className="links">
+                
+            </div>
+
+            <div className="bottom">
+                
+            </div>
+        </div>
     )
 }
 
