ed089ea1d011f6467e6d5d45f57c08c951678c95
jessymatta <jessy.w.matta@gmail.com>
2022-11-08 23:21:28 +0200

    Frontend ⇄ Add the labels components to the sidebar component

--- classifyai-frontend/src/components/sidebar/index.tsx
+++ classifyai-frontend/src/components/sidebar/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import Logo from "../logo/";
+import SidebarLabel from '../sidebarLabel';
 import "./index.scss"
 import { SidebarProps } from './sidebar';
+import { MdOutlineDashboard } from "react-icons/md";
+import { RiFileExcel2Line, RiCustomerService2Line } from "react-icons/ri";
+import { MdSupervisorAccount } from "react-icons/md";
+import { IoCallOutline } from "react-icons/io5";
+import { FiLogOut } from "react-icons/fi";
 
 const Sidebar = ({role, username}:SidebarProps) => {
+    const logout = () => {
+        localStorage.clear();
+        window.location.href = "/login";
+    }
+
     return (
         <div className="sidebar">
             <div className="sidebar__top">
@@ -12,13 +23,20 @@ const Sidebar = ({role, username}:SidebarProps) => {
                 <p className='sidebar__role'>{role}</p>
                 <p className="sidebar__username">{username? `@${username}`: ""}</p>
 
-            <div className="links">
+            <div className="sidebar__links">
+                {/* //TODO: add links  */}
+                <SidebarLabel icon={<MdOutlineDashboard size={30}/>} labelName='dashboard'/>
+                <SidebarLabel icon={<MdSupervisorAccount size={30}/>} labelName='supervisors'/>
+                <SidebarLabel icon={<RiCustomerService2Line size={30}/>} labelName='operators'/>
+                <SidebarLabel icon={<IoCallOutline size={30}/>} labelName='calls'/>
+                <SidebarLabel icon={<RiFileExcel2Line size={30}/>} labelName='scripts'/>
+
+                <div className="sidebar__logout">
+                <SidebarLabel onClick={logout} icon={<FiLogOut size={30}/>} labelName='logout'/>
+                </div>
                 
             </div>
 
-            <div className="bottom">
-                
-            </div>
         </div>
     )
 }
