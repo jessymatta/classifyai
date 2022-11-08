969cf854fa5991c3fef9e2a401633eb60de2957f
jessymatta <jessy.w.matta@gmail.com>
2022-11-09 00:11:01 +0200

    Frontend ⇄ Add the footer component

---
+++ classifyai-frontend/src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p className="footer__content">©2022 CLASSIFYAI | All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
