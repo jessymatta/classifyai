b1ae2cce3456a2fc8c760ce9aa9a38cd4fa174c0
jessymatta <jessy.w.matta@gmail.com>
2022-11-09 12:18:48 +0200

    Frontend ⇄ Add and style the icons in the header

--- classifyai-frontend/src/components/header/index.tsx
+++ classifyai-frontend/src/components/header/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import "./index.scss";
 import { queryClient } from '../../App';
 import { UserDetails } from '../../routes/UserInterface';
+import {MdOutlineLightMode} from "react-icons/md";
+import {CiDark} from "react-icons/ci";
 
 const Header = () => {
     const loggedInUser: UserDetails = queryClient.getQueryCache().find(['USER_LOGGED_IN'])?.state.data as UserDetails;
@@ -10,7 +12,7 @@ const Header = () => {
     return (
         <div className='header'>
             <div className="header__wrapper">
-                <div className="header__toggleBtn">B</div>
+                <div className="header__toggleBtn"><CiDark/><MdOutlineLightMode /></div>
                 {loogedInUserProfilePic ? <img src={`http://127.0.0.1:8000/images/profile_pictures/${loogedInUserId}/${loogedInUserProfilePic}`} className="header__pp" alt="pp" /> : ""}
             </div>
         </div>
