11d020bef824fae936d35beb33367c988b40d50e
jessymatta <jessy.w.matta@gmail.com>
2022-11-10 21:32:11 +0200

    Frontend ⇄ Define the interface for the BarChart props

---
+++ classifyai-frontend/src/components/barChartCard/BarChartProps.ts
@@ -0,0 +1,8 @@
+export interface BarChartProps {
+    title: string,
+    subtitle?: string,
+    labels: Array<string>,
+    positivePctArray: Array<number>,
+    negativePctArray: Array<number>,
+    neutralPctArray: Array<number>
+}
