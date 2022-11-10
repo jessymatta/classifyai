881e4e40b5472763483d78f32bf2a46e96f40b9b
jessymatta <jessy.w.matta@gmail.com>
2022-11-10 20:16:54 +0200

    Frontend ⇄ Replace sidebar labels rendering logic by calling the predefined object and mapping the results

--- classifyai-frontend/src/components/sidebar/index.tsx
+++ classifyai-frontend/src/components/sidebar/index.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
+import "./index.scss";
 import Logo from "../logo/";
 import SidebarLabel from '../sidebarLabel';
-import "./index.scss";
-import { MdOutlineDashboard } from "react-icons/md";
-import { RiFileExcel2Line, RiCustomerService2Line } from "react-icons/ri";
-import { MdSupervisorAccount } from "react-icons/md";
-import { IoCallOutline } from "react-icons/io5";
 import { FiLogOut } from "react-icons/fi";
 import { UserDetails } from '../../routes/UserInterface';
 import { queryClient } from '../../App';
 import { getRoleNameByValue } from '../../helpers/getRoleTitle';
-
-
-const ROLES = {
-    "Super_Supervisor": 1,
-    "Supervisor": 2,
-    "Operator": 3
-}
+import { sidesbarLabels } from "../../constants/sidebarEnum"
+import { ROLES } from "../../constants/roles"
 
 const Sidebar = () => {
     const loggedInUser: UserDetails = queryClient.getQueryCache().find(['USER_LOGGED_IN'])?.state.data as UserDetails;
-    const loogedInUserRole = loggedInUser?.role_id;
-    const loogedInUserUsername = loggedInUser?.username;
+    const loggedInUserRole = loggedInUser?.role_id;
+    const loggedInUserUsername = loggedInUser?.username;
 
     const logout = () => {
         localStorage.clear();
@@ -33,16 +24,14 @@ const Sidebar = () => {
             <div className="sidebar__top">
                 <Logo logoType={"sidebar__logo"} />
             </div>
-            <p className='sidebar__role'>{getRoleNameByValue(ROLES, loogedInUserRole)}</p>
-            <p className="sidebar__username">{loogedInUserUsername ? `@${loogedInUserUsername}` : ""}</p>
+            <p className='sidebar__role'>{getRoleNameByValue(ROLES, loggedInUserRole)}</p>
+            <p className="sidebar__username">{loggedInUserUsername ? `@${loggedInUserUsername}` : ""}</p>
 
             <div className="sidebar__links">
 
-                <SidebarLabel icon={<MdOutlineDashboard size={30} />} labelName='dashboard' />
-                {loogedInUserRole === ROLES.Super_Supervisor ? <SidebarLabel icon={<MdSupervisorAccount size={30} />} labelName='supervisors' /> : ""}
-                {(loogedInUserRole === ROLES.Super_Supervisor) || (loogedInUserRole === ROLES.Supervisor) ? <SidebarLabel icon={<RiCustomerService2Line size={30} />} labelName='operators' /> : ""}
-                {(loogedInUserRole === ROLES.Super_Supervisor) || (loogedInUserRole === ROLES.Supervisor) ? <SidebarLabel icon={<IoCallOutline size={30} />} labelName='calls' /> : ""}
-                <SidebarLabel icon={<RiFileExcel2Line size={30} />} labelName='scripts' />
+                {sidesbarLabels[loggedInUserRole].map((label) => (
+                    <SidebarLabel linkTo={label.linkTo} icon={label.icon} labelName={label.labelName} />
+                ))}
 
                 <div className="sidebar__logout">
                     <SidebarLabel onClick={logout} icon={<FiLogOut size={30} />} labelName='logout' />
