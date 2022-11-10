a7d3af073c1bbb2076eda9c0016f08d69c84aad0
jessymatta <jessy.w.matta@gmail.com>
2022-11-10 21:57:28 +0200

    Frontend ⇄ Add helpers fcts to the DoghnutChartCard component

---
+++ classifyai-frontend/src/helpers/doughnutChartHelpers.ts
@@ -0,0 +1,34 @@
+import { ChartOptions } from 'chart.js';
+
+export const chartData = (positivePct: number, negativePct: number, neutralPct: number) => {
+    const dataToReturn = {
+        labels: ['Positive', 'Negative', 'Neutral'],
+        datasets: [
+            {
+                data: [positivePct, negativePct, neutralPct],
+                backgroundColor: [
+                    '#41D847',
+                    '#EF1C26',
+                    '#FFD000',
+                ],
+                borderWidth: 0,
+            },
+
+        ],
+    }
+    return dataToReturn;
+}
+
+export const options: ChartOptions<'doughnut'> = {
+    plugins: {
+        legend: {
+            position: 'left',
+            align: "end",
+            labels: {
+                usePointStyle: true,
+                pointStyle: 'circle',
+                padding: 15,
+            }
+        }
+    },
+}
