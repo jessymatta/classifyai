3714acbc613097e016f44f102a377a4d6106846a
jessymatta <jessy.w.matta@gmail.com>
2022-11-13 16:10:07 +0200

    Frontend ⇄ Add a method to parse and return BarChart data

--- classifyai-frontend/src/helpers/barChartHelpers.ts
+++ classifyai-frontend/src/helpers/barChartHelpers.ts
@@ -22,3 +22,28 @@ export const barChartData = (arrayLabels: Array<string>, arrayPositivePct: Array
     return dataToReturn;
 }
 
+export const parseAndGetBarChartData = (data: object | any) => {
+    const labels = Object.keys(data);
+    const positiveData = []
+    const negativeData = []
+    const neutralData = []
+    for (const date in data) {
+        for (const dailyEval in data[date]) {
+
+            switch (dailyEval) {
+                case "average_positive_pct":
+                    positiveData.push(data[date][dailyEval])
+                    break;
+                case "average_negative_pct":
+                    negativeData.push(data[date][dailyEval])
+                    break;
+                case "average_neutral_pct":
+                    neutralData.push(data[date][dailyEval])
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    return [labels, positiveData, negativeData, neutralData]
+}
