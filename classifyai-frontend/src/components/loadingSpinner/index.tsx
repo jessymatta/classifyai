f565e8edfe9863fe7f5f31d44276bbc8e6b1adc0
jessymatta <jessy.w.matta@gmail.com>
2022-11-17 19:12:24 +0200

    Frontend ⇄ Add Loading spinner component (w/ success message)

---
+++ classifyai-frontend/src/components/loadingSpinner/index.tsx
@@ -0,0 +1,35 @@
+import "./index.scss"
+import { AiOutlineCheckCircle } from "react-icons/ai"
+import { LoadingSpinnerProps } from "./LoadingSpinnerProps"
+
+const LoadingSpinner = ({
+    topMsg,
+    bottomMsg,
+    loading, success,
+    successMsgTop,
+    successMsgBottom }: LoadingSpinnerProps) => {
+
+    return (
+
+        <div className='spinner'>
+            {loading &&
+                <>
+                    <p>{topMsg}</p>
+                    <div className="spinner-container">
+                        <div className="loading-spinner"></div>
+                    </div>
+                    <p>{bottomMsg}</p>
+                </>
+            }
+
+            {success &&
+                <div className='sucessMsg__container'>
+                    <AiOutlineCheckCircle className='success__icon' />
+                    <h6 className='success__msg'>{successMsgTop}</h6>
+                    <p>{successMsgBottom}</p>
+                </div>}
+        </div>
+    )
+}
+
+export default LoadingSpinner
