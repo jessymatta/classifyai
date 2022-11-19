ba53497cc2e927136c0fc71ab4cd5bfee3d10090
jessymatta <jessy.w.matta@gmail.com>
2022-11-19 14:03:19 +0200

    Frontend ⇄ Add the components to the supervisors page

--- classifyai-frontend/src/pages/supervisors/index.tsx
+++ classifyai-frontend/src/pages/supervisors/index.tsx
@@ -1,15 +1,67 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Footer from '../../components/footer'
 import Header from '../../components/header'
 import Sidebar from '../../components/sidebar'
+import Table from '../../components/table'
+import TitleComponent from '../../components/titleBar'
 import DashboardHOC from '../../hoc/DashboardHOC'
+import Modal from '../../components/employeeModal'
+import Pagination from '../../components/pagination'
+import AddInfoModalHOC from '../../hoc/addInfoModalHOC'
+import { useGetAllSupervisors } from '../../query/supervisors/useSupervisors'
+
 
 
 const Supervisors = () => {
+    const [supervisors, setSupervisors] = useState([])
+    const { data: allSupervisors, isSuccess: allSupervisorsSuccess } = useGetAllSupervisors()
+    const [currentPage, setCurrentPage] = useState(1)
+    const [usersPerPage] = useState(7)
+    const [openAddUserModal, setOpenAddUserModal] = useState(false)
+    const indexOfLastUser = currentPage * usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - usersPerPage;
+    const currentUsers = supervisors.slice(indexOfFirstUser, indexOfLastUser)
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
+
+    useEffect(() => {
+        if (allSupervisorsSuccess) {
+            setSupervisors(allSupervisors.data)
+        }
+    }, [allSupervisors])
     return (
         <DashboardHOC>
             <Sidebar />
-            <Header /><div>Supervisors Content here</div>
+            <Header />
+            <div>
+                <TitleComponent
+                    title={"Supervisors"}
+                    btnText={"add supervisor"}
+                    onClick={() => setOpenAddUserModal(true)}
+                />
+
+                <Table
+                    headers={["Supervisor", "Email", "Joining Date", "Actions"]}
+                    rowsData={currentUsers}
+                />
+
+                <Pagination
+                    usersPerPage={usersPerPage}
+                    totalUsers={supervisors.length}
+                    paginate={paginate}
+                />
+
+                <AddInfoModalHOC
+                    open={openAddUserModal}
+                    onClose={() => setOpenAddUserModal(false)}
+                    modalTitle={"Add Supervisor"} >
+                    <Modal
+                        onSuccess={() => setOpenAddUserModal(false)}
+                        supervisor={true} 
+                    />
+                </AddInfoModalHOC>
+
+
+            </div>
             <Footer />
         </DashboardHOC>
     )
