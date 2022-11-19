95177ff56a8ac07a7e2ba2777aa072e51f226a19
jessymatta <jessy.w.matta@gmail.com>
2022-11-19 14:44:57 +0200

    Frontend ⇄ Add operator info modal to the cardtype1 component

--- classifyai-frontend/src/components/cardType1/index.tsx
+++ classifyai-frontend/src/components/cardType1/index.tsx
@@ -1,35 +1,49 @@
-import React from 'react'
 import "./index.scss"
+import { useState } from 'react'
 import Button from '../button'
 import { CardProps } from "./cardType1Props"
+import OperatorStatsModal from '../operatorStatsModal'
 
-const Card = ({ title, firstName, lastName, username, emoji, percentage, image, type }: CardProps) => {
+const Card = ({ title, firstName, lastName, username, emoji, percentage, image, type, id }: CardProps) => {
+    const [isOperatorStatsModalActive, setIsOperatorStatsModalActive] = useState(false);
     return (
-        <div className='card'>
+        <>
+            <div className='card'>
 
-            <p className='card__title'>{title}</p>
+                <p className='card__title'>{title}</p>
 
-            <div className="card__container">
+                <div className="card__container">
 
-                <div className="card__container--left">
-                    <p className='card__subtitle margin-top'>{`${firstName} ${lastName}`}</p>
-                    <p className='card__subtitle'>{`@ ${username}`}</p>
+                    <div className="card__container--left">
+                        <p className='card__subtitle margin-top'>{`${firstName} ${lastName}`}</p>
+                        <p className='card__subtitle'>{`@ ${username}`}</p>
 
-                    <div className="card__container--left--percentage">
-                        <img className="card__emoji" src={emoji} alt="emoji" />
-                        <p className={`card__percentage ${type}`}>{`${percentage}%`}</p>
+                        <div className="card__container--left--percentage">
+                            <img className="card__emoji" src={emoji} alt="emoji" />
+                            <p className={`card__percentage ${type}`}>{`${percentage}%`}</p>
+                        </div>
+
+                        <Button
+                            text={"view profile"}
+                            classNames={["button--fixedwidth", "button--red"]}
+                            onClick={() => setIsOperatorStatsModalActive(true)}
+                        />
                     </div>
 
-                    <Button text={"view profile"} classNames={["button--fixedwidth", "button--red"]} />
-                </div>
+                    <div className="card__container--right">
+                        <img className={`card__image--${type}`} src={image} alt="Trophy" />
+                    </div>
 
-                <div className="card__container--right">
-                    <img className={`card__image--${type}`} src={image} alt="Trophy" />
                 </div>
 
             </div>
+            {isOperatorStatsModalActive &&
+                <OperatorStatsModal
+                    onClose={() => { setIsOperatorStatsModalActive(false) }}
+                    id={id} />
+            }
 
-        </div>
+        </>
     )
 }
 
