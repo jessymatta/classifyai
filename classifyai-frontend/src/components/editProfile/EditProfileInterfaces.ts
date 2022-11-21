ff1e8a04dffc46d37eceb1374973b01a1b4dceed
jessymatta <jessy.w.matta@gmail.com>
2022-11-21 10:43:20 +0200

    Frontend ⇄ Define the interfaces for the editProfile component

---
+++ classifyai-frontend/src/components/editProfile/EditProfileInterfaces.ts
@@ -0,0 +1,23 @@
+import { UserDetails } from "../../routes/UserInterface"
+
+export interface EditProfileModalProps {
+    employee: UserDetails;
+    onClose: () => void;
+}
+
+export interface EditUserFormProps {
+    first_name: string;
+    last_name: string;
+    email: string;
+    username: string;
+    dob: string;
+    profile_pic_base64: string;
+}
+
+export interface EditUserFormErrors {
+    first_name?: string;
+    last_name?: string;
+    email?: string;
+    username?: string;
+    dob?: string;
+}
