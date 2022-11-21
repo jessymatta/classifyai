0c53518117684a0ccf3e741f1a4dd567818dfbf0
jessymatta <jessy.w.matta@gmail.com>
2022-11-21 11:21:59 +0200

    Frontend ⇄ Add the logic to display add btns w.r.t logged in user role

--- classifyai-frontend/src/components/titleBar/index.tsx
+++ classifyai-frontend/src/components/titleBar/index.tsx
@@ -1,8 +1,10 @@
 import "./index.scss"
 import Button from "../button"
 import { TitleProps } from "./TitleProps"
+import { queryClient } from '../../App';
 
 const TitleComponent = ({ title, btnText, onClick, modal }: TitleProps) => {
+    const userRole = queryClient.getQueryCache().find(['USER_LOGGED_IN_ROLE'])?.state.data
     return (
 
         <div className={modal ? 'modal_title' : "title_container"}>
@@ -11,7 +13,7 @@ const TitleComponent = ({ title, btnText, onClick, modal }: TitleProps) => {
             {modal &&
                 <p className='arrow' onClick={onClick}>X</p>}
 
-            {btnText &&
+            {(userRole === "Super Supervisor") && btnText &&
                 <Button
                     text={btnText}
                     classNames={["button--title"]}
