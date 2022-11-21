355c95f8ca06694574b87490a51d6b66a862b3c4
jessymatta <jessy.w.matta@gmail.com>
2022-11-21 18:06:13 +0200

    Frontend ⇄ Change the import statement of the base url to another one

--- classifyai-frontend/src/components/tableRowUserInfo/index.tsx
+++ classifyai-frontend/src/components/tableRowUserInfo/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./index.scss"
 import DummyPP from "../../assets/images/dummy__pp.svg"
 import { TableRowUserInfoProps } from "./TableRowUserProps"
-import { BASE_URL_PP } from "../../constants/urls"
+import Config from "../../constants/config.json"
 
 const TableRowUserInfo = ({ ppPath, firstName, lastName, username, id }: TableRowUserInfoProps) => {
 
@@ -10,7 +10,7 @@ const TableRowUserInfo = ({ ppPath, firstName, lastName, username, id }: TableRo
         <div className='user__container'>
             <div className="user__info">
                 {ppPath ?
-                    <img src={`${BASE_URL_PP}/${id}/${ppPath}`} className="user__pp" alt="pp" />
+                    <img src={`${Config.BASE_URL_PP}/${id}/${ppPath}`} className="user__pp" alt="pp" />
                     :
                     <img src={DummyPP} className="user__pp" alt="pp" />}
                 <div className="user__info--right">
