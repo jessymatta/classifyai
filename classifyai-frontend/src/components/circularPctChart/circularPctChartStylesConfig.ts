9c1095f516e39381abcb2f37ed245ba1ca1a4ed4
jessymatta <jessy.w.matta@gmail.com>
2022-11-22 11:34:45 +0200

    Frontend ⇄ Extract the CircularPctChart styles config to a separate file

---
+++ classifyai-frontend/src/components/circularPctChart/circularPctChartStylesConfig.ts
@@ -0,0 +1,8 @@
+import { buildStyles } from "react-circular-progressbar";
+
+export const circularPctStyles = buildStyles({
+    strokeLinecap: "butt",
+    textColor: "#949494",
+    pathColor: "#41D847",
+    trailColor: "#949494"
+});
