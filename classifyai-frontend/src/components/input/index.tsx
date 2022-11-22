340cdf33757efabe648e7ce17f0216fa4a3bc5db
jessymatta <jessy.w.matta@gmail.com>
2022-11-22 16:06:32 +0200

    Frontend ⇄ Add more properties to the input tag component

--- classifyai-frontend/src/components/input/index.tsx
+++ classifyai-frontend/src/components/input/index.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import { useState } from "react";
 import "./index.scss";
 import { InputProps } from "./input";
 
-const Input = ({ type, label, required, defaultValue, name, onChange }: InputProps) => {
+const Input = ({ type, label, required, defaultValue, name, onChange, errorMessage, pattern }: InputProps) => {
+    const [focused, setFocused] = useState(false);
+    const handleFocus = () => {
+        setFocused(true);
+    }
+
     return (
         <div className="form">
-            <input type={type} name={name} defaultValue={defaultValue} className="form__input" id="input" required={required} autoComplete="off" placeholder=" " onChange={onChange} />
+
+            <input
+                pattern={pattern}
+                type={type}
+                name={name}
+                defaultValue={defaultValue}
+                className="form__input"
+                id="input"
+                required={required}
+                autoComplete="off"
+                placeholder=" "
+                onChange={onChange}
+                onBlur={handleFocus}
+                //@ts-ignore
+                focused={focused.toString()}
+                //@ts-ignore
+                onFocus={() => name === 'confirmPass' && setFocused(true)}
+            />
+
             <label htmlFor="input" className="form__label">{label}</label>
+            <span>{errorMessage}</span><br />
         </div>
     )
 }
--- classifyai-frontend/src/components/input/input.ts
+++ classifyai-frontend/src/components/input/input.ts
@@ -1,8 +1,12 @@
 export interface InputProps {
     type: string;
     label: string;
-    defaultValue: string;
+    defaultValue?: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    required?: boolean,
-    name:string;
+    required?: boolean;
+    name: string;
+    errorMessage?: string;
+    pattern?: string;
+    onBlur?: React.FocusEvent<HTMLElement>;
+    focused?: string | undefined;
 }
