1fb680f398e76dbc29533696b33bf3b1fcdd5631
jessymatta <jessy.w.matta@gmail.com>
2022-11-22 11:19:21 +0200

    Frontend ⇄ Format the files of the CardType2 component folder

--- classifyai-frontend/src/components/cardType2/index.tsx
+++ classifyai-frontend/src/components/cardType2/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import "./index.scss"
-import { CardType2Props } from './CardType2Props'
+import "./index.scss";
+import { CardType2Props } from "./CardType2Props";
 
 const SmallCard = ({ stats, title, image, subtitle, shadow }: CardType2Props) => {
     return (
-        <div className={shadow? 'smallcard shadow': 'smallcard'}>
+        <div className={shadow ? 'smallcard shadow' : 'smallcard'}>
 
             <div className="smallcard__left">
                 <img className='smallcard__left--img' src={image} alt="phone" />
@@ -20,4 +19,4 @@ const SmallCard = ({ stats, title, image, subtitle, shadow }: CardType2Props) =>
     )
 }
 
-export default SmallCard
+export default SmallCard;
