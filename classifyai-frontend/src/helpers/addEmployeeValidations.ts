f817357a1d747b9eccf47b9d1914f13edc77b8c3
jessymatta <jessy.w.matta@gmail.com>
2022-11-22 12:52:17 +0200

    Frontend ⇄ Add date of birth validations to the add employee form

--- classifyai-frontend/src/helpers/addEmployeeValidations.ts
+++ classifyai-frontend/src/helpers/addEmployeeValidations.ts
@@ -1,4 +1,5 @@
 import { AddUserFormErrors, AddUserFormProps } from "../components/employeeModal/ModalAddUsers";
+const today = Date.now();
 
 export function validateAddUserForm({
     firstName, lastName, username, email, dob, password, confirmPass }: AddUserFormProps) {
@@ -8,9 +9,12 @@ export function validateAddUserForm({
         errors.password = "Password must be at least 7 characters long ";
     }
 
-    if (password && confirmPass && (password === confirmPass) == false) {
+    if (password && confirmPass && (password === confirmPass) === false) {
         errors.confirmPass = "The passwords do not match"
     }
 
+    if (dob && Date.parse(dob) > today) {
+        errors.dob = `Yo! Employee cannot be born in the future`
+    }
     return errors;
 }
